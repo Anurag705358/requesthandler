56d2d16c0650060805f7b4bdbd5a6a331b62585c
Anurag705358 <anurag705358@gmail.com>
2020-09-30 19:23:32 +0530

    first

--- src/main/java/com/example/requesthandler/request/RequestService.java
+++ src/main/java/com/example/requesthandler/request/RequestService.java
@@ -11,41 +11,40 @@ import com.example.requesthandler.bean.Request;
 @Service
 public class RequestService {
 	private Lock lock = new ReentrantLock();;
-	private HashMap<Integer,Request> requestDataMap=new HashMap<>();
-	
-	private HashMap<Integer,String> requestStatusMap=new HashMap<>();
-	
-	public  void processRequest(Request req) throws Exception {
-		int reqId=req.getRequestId();
-		if(lock.tryLock()) {
-			if(!requestDataMap.containsKey(reqId)&&!requestStatusMap.containsKey(reqId)){
-		
-			requestDataMap.put(reqId, req);
-			requestStatusMap.put(reqId, "processing");
-		}
+	private HashMap<Integer, Request> requestDataMap = new HashMap<>();
 
-		Thread.sleep(10000); // for testing multiple hit
-		}else {
+	private HashMap<Integer, String> requestStatusMap = new HashMap<>();
+
+	public void processRequest(Request req) throws Exception {
+		int reqId = req.getRequestId();
+		if (lock.tryLock()) {
+			if (!requestDataMap.containsKey(reqId) && !requestStatusMap.containsKey(reqId)) {
+
+				requestDataMap.put(reqId, req);
+				requestStatusMap.put(reqId, "processing");
+			}
+
+			Thread.sleep(10000); // for testing multiple hit
+
+			lock.unlock();
+
+		} else {
 			throw new Exception("Already processing");
 		}
-		
+
 	}
-	
-	
-	public  void endRequest(Request req) throws Exception {
-		int reqId=req.getRequestId();
-			if(!requestDataMap.containsKey(reqId)&&!requestStatusMap.containsKey(reqId)){
-				throw new Exception("Data don't exist");
+
+	public void endRequest(Request req) throws Exception {
+		int reqId = req.getRequestId();
+		if (!requestDataMap.containsKey(reqId) && !requestStatusMap.containsKey(reqId)) {
+			throw new Exception("Data don't exist");
 		}
-			
-		if("processed".equals(requestStatusMap.get(reqId))){
+
+		if ("processed".equals(requestStatusMap.get(reqId))) {
 			throw new Exception("Already processed");
-	}
+		}
 		requestStatusMap.put(reqId, "processed");
-		
-		
+
 	}
-	
+
 }
-
-
